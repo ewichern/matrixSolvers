9118dc11c2e96f5a66ec05500dbd453d94ff29f4
ewichern <ewichern@cs.odu.edu>
2015-01-21 23:30:57 -0500

    Initial commit

---
+++ matrixGenerator.cpp
@@ -0,0 +1,18 @@
+/*
+ * matrixGenerator.cpp
+ *
+ *  Created on: Jan 21, 2015
+ *      Author: erik
+ */
+
+#include "matrixGenerator.h"
+
+matrixGenerator::matrixGenerator() {
+	// TODO Auto-generated constructor stub
+
+}
+
+matrixGenerator::~matrixGenerator() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ matrixGenerator.h
@@ -0,0 +1,20 @@
+/*
+ * matrixGenerator.h
+ *
+ *  Created on: Jan 21, 2015
+ *      Author: erik
+ */
+
+#ifndef MATRIXGENERATOR_H_
+#define MATRIXGENERATOR_H_
+
+/*
+ *
+ */
+class matrixGenerator {
+public:
+	matrixGenerator();
+	virtual ~matrixGenerator();
+};
+
+#endif /* MATRIXGENERATOR_H_ */
